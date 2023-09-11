4c7935eed2a766435ff77aaf9d8749b0f0c61827
iam5abhi <support@codesoftic.com>
2023-09-11 18:50:50 +0530

    main

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
           <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         </Head>
         <Header />
-        <GoogleAnalytics measurementId="G-KDLXEFRJVX" />
+        <GoogleAnalytics measurementId="G-145K8ZM6EW" />
         <Component {...pageProps} />
       </> 
   )
