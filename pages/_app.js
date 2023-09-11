7cc8861c304aaa6716fe5eb6800abb4a3343f724
iam5abhi <support@codesoftic.com>
2023-09-11 11:36:11 +0530

    main

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,16 @@ function MyApp({ Component, pageProps }) {
         <Head>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
           <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4914031929849543"
+          crossorigin="anonymous"></script>
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-145K8ZM6EW"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-145K8ZM6EW');
+          </script>
         </Head>
         <Header />
         <Component {...pageProps} />
